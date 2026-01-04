424e807afb756e130c49a39ca32aa03fc88a7415
Vagner-Alves-backend <vagner.alves.dev@gmail.com>
2026-01-03 22:54:37 -0300

    ---Novo inicio, limitações de movimentação das peças Fase inicial.

--- _Jogos/_Torre-de-Haro/_Main.C
+++ _Jogos/_Torre-de-Haro/_Main.C
@@ -33,49 +33,86 @@ void buffer() {
 
 
 void iniciar_jogo(int matriz[][3]) {
-    int x = 0;
-    int y = 0;
-    int pecca = 0;
+    int eixo_x = 0;
+    int eixo_y = 0;
 
-    int pode_mover = 0;
+    int pecca = 0;
+    int torre = 0;
+    int movimentos = 0;
+    int movimento_regular = 0;
+    int vitoria = 0;
+
+    int tecla = 0;
 
     do {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                printf("%d ", matriz[i][j]);
+        system("cls");
+        // a função a seguir será responsavel por desenhar a torre---.
+        for (int eixo_x_for = 0; eixo_x_for < 3; eixo_x_for++) {
+            printf("\t\t");
+            for (int eixo_y_for = 0; eixo_y_for < 3; eixo_y_for++) {
+                printf("%d ", matriz[eixo_x_for][eixo_y_for]);
             }
             printf("\n");
-        }   
-        printf("qual pecca voce deseja mover? ");
-        scanf("%d", &pecca);
+        }
 
+        // a função a seguir sera responsavel por coletar a peça que o jogador deseja movimentar...
+        movimento_regular = 0;
         do {
-            printf("para qual torre? ");
-            scanf("%d", &y);
+            printf("---Qual pecca voce deseja movimentar?\n");
+            printf("[1]  *  \n[2] *** \n[3]*****\n............\n..: ");
+            scanf("%d", &pecca);
             buffer();
 
-            for (int i = 2; i >= 0; i--) {
-                if (matriz[i][y] == 0) {
-                    pode_mover = 1;
-                    x = i;
-                    i = -1;
+            for (int eixo_x_for = 0; eixo_x_for < 3; eixo_x_for++) {
+                for (int eixo_y_for = 0; eixo_y_for < 3; eixo_y_for++) {
+                    if (matriz[eixo_x_for][eixo_y_for] == pecca) {
+                        if (eixo_x_for == 0) {
+                            movimento_regular = 1;
+                            eixo_x = eixo_x_for;
+                        } else if (matriz[eixo_x_for -1][eixo_y_for] == 0) {
+                            movimento_regular = 1;
+                            eixo_x = eixo_x_for;
+                        }
+                    }
                 }
             }
 
-            if (pode_mover != 1) {
-                printf("Movimento inrregular... Escolha um novo destino---\n\n");
+            if (movimento_regular != 1) {
+                printf("\t---Movimento inrregular, escolha uma nova pecca...\n\n");
             }
-        } while (pode_mover != 1);
-        
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (matriz[i][j] == pecca) {
-                    matriz[i][j] = 0;
-                } 
-            }
-        } 
+        } while (movimento_regular != 1);
+        printf("\t\tx = %d\tpecca = %d\n", eixo_x, pecca);
 
-        matriz[x][y] = pecca;
-        system("cls");
-    } while (x != 201);
+
+        // a função a seguir permite o jogador escolher a torre para onde deseja mandar a peça selecionada...
+        movimento_regular = 0;
+        do {
+            printf("---Para qual torre deseja realocar a pecca? ");
+            scanf("%d", &torre);
+            buffer();
+
+            for (int eixo_y_for = 2; eixo_y_for >= 0; eixo_y_for--) {
+                if (matriz[eixo_y_for][torre -1] == 0) {
+                    if (eixo_y_for == 2) {
+                        movimento_regular = 1;
+                        eixo_y = eixo_y_for;
+                        eixo_y_for = -1;
+                    } else if (matriz[eixo_y_for][torre -2] > pecca) {
+                        movimento_regular = 1;
+                        eixo_y = eixo_y_for;
+                        eixo_y_for = -1;
+                    }
+                }
+            }
+
+            if (movimento_regular != 1) {
+                printf("\t---Movimento inrregular, escolha uma nova posiccao...\n\n");
+            }
+        } while (movimento_regular != 1);
+        printf("\t\ttorre [%d], torre_calc[%d], y = %d\n", torre, torre-1, eixo_x);
+        
+
+
+        vitoria = 1;
+    } while (vitoria != 1);
 } 
